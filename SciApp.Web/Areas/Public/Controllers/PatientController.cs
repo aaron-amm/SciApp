446170c136e61628e32e78d3cb1130b9af3d0fde
theeranitp <theeranitp@gmail.com>
2016-08-29 00:42:40 +0700

    added patient controller and its unit test

---
+++ SciApp.Web/Areas/Public/Controllers/PatientController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SciHospital.WebApp.Areas.Public.Controllers
+{
+    public class PatientController : Controller
+    {
+        private IPatientRepository patientRepository;
+        public PatientController(IPatientRepository patientRepository)
+        {
+            this.patientRepository = patientRepository;
+        }
+
+        public ActionResult UpdatePatient(int id, string firstName, string lastName, DateTime? dateOfBirth)
+        {
+            var patient = patientRepository.GetPatient(id);
+
+            patient.FirstName = firstName ?? patient.FirstName;
+            patient.LastName = lastName ?? patient.LastName;
+            patient.DateOfBirth = dateOfBirth ?? patient.DateOfBirth;
+
+            return View(patient);
+        }
+
+    }
+}
---
+++ Tests/SciApp.Web.Test/PatientControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using SciHospital.WebApp.Areas.Public.Controllers;
+using System;
+using System.Web.Mvc;
+
+namespace SciApp.Web.Test
+{
+    [TestFixture]
+    public class PatientControllerTest
+    {
+        IPatientRepository repository;
+        PatientController controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            repository = MockRepository.GenerateMock<IPatientRepository>();
+            controller = new PatientController(repository);
+        }
+
+        [Test]
+        public void UpdatePatient_OnlyNewFirstNameNotNull_PatientFirstNameUpdated()
+        {
+            var existingPatient = new Patient()
+            {
+                Id = 1,
+                FirstName = "Aaron",
+                LastName = "Amm",
+                DateOfBirth = new DateTime(2000, 3, 16)
+            };
+
+            repository.Stub(r => r.GetPatient(existingPatient.Id)).Return(existingPatient);
+
+            var newFirstName = "NewAaron";
+            var actionResult = (ViewResult)controller.UpdatePatient(id: existingPatient.Id, firstName: newFirstName, lastName: null, dateOfBirth: null);
+            var updatedPatient = (Patient)actionResult.Model;
+
+            Assert.AreEqual(newFirstName, updatedPatient.FirstName);
+            Assert.AreEqual(existingPatient.LastName, updatedPatient.LastName);
+            Assert.AreEqual(existingPatient.DateOfBirth, updatedPatient.DateOfBirth);
+        }
+    }
+}
