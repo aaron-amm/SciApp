d0d60849b6d3034dbcbc72f653a28a44f76af41c
aaronamm <theeranitp@gmail.com>
2016-09-14 13:18:06 +0700

    unit testing for space separated values helper class

---
+++ SciApp.Utility/SeparatedValueHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SciApp.Utility
+{
+    public static class SeparatedValueHelper
+    {
+        private static readonly string[] EmptyArray = new string[0];
+        public static string[] SpaceSeparatedToArray(this string separatedValue)
+        {
+            if (string.IsNullOrEmpty(separatedValue))
+            {
+                return EmptyArray;
+            }
+
+            return separatedValue.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static string ArrayToSpaceSeparated(this string[] stringArray)
+        {
+            if(stringArray ==null || stringArray.Length == 0)
+            {
+                return string.Empty;
+            }
+           return string.Join(" ", stringArray);
+        }
+
+
+
+    }
+}
---
+++ Tests/SciApp.Utility.Test/SeparatedValueHelperTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace SciApp.Utility.Test
+{
+    [TestFixture]
+    public class SeparatedValueHelperTest
+    {
+        [Test]
+         public void Test()
+        {
+            var spaceSeparatedValue = "aa bb cc";
+          var arrayValue=  spaceSeparatedValue.SpaceSeparatedToArray();
+            Assert.AreEqual(3, arrayValue.Length);
+
+        }
+
+        [Test]
+        public  void Test2()
+        {
+
+            var arrayValue= new []{ "aa", "bb", "cc"};
+            var stringValue = arrayValue.ArrayToSpaceSeparated();
+            Assert.AreEqual("aa bb cc", stringValue);
+        }
+    }
+}
