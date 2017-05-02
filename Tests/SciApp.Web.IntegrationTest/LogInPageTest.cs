8caf200c237149a4e76a6b508cacc44ec71cb454
aaronamm <theeranitp@gmail.com>
2017-05-02 07:36:20 +0700

    initial setup Selenium grid

--- Tests/SciApp.Web.IntegrationTest/LogInPageTest.cs
+++ Tests/SciApp.Web.IntegrationTest/LogInPageTest.cs
@@ -1,24 +1,48 @@
-﻿using Bumblebee.Extensions;
+﻿using System;
+using System.Threading;
+using Bumblebee.Extensions;
 using Bumblebee.Interfaces;
 using Bumblebee.Setup;
 using Bumblebee.Setup.DriverEnvironments;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 
 namespace SciApp.Web.IntegrationTest
 {
+
+    public class GridDriver : IDriverEnvironment
+    {
+        public IWebDriver CreateWebDriver()
+        {
+            const string node = "http://192.168.1.105:4444/wd/hub";
+            var cap = DesiredCapabilities.Chrome();
+            //cap.SetCapability("browserName","chrome");
+            //cap.SetCapability("platform","WIN10");
+            //var driver = new RemoteWebDriver(new Uri(node), cap);
+            //return driver;
+            //var option = new ChromeOptions();
+            //option.BinaryLocation = @"C:\projects\SciApp\packages\Selenium.WebDriver.ChromeDriver.2.29.0\driver\win32\chromedriver.exe";
+            //var capabilities = option.ToCapabilities();
+            //var commandTimeout = TimeSpan.FromMinutes(5);
+            var driver = new RemoteWebDriver(new Uri(node), cap);
+            return driver;
+        }
+    }
     [TestFixture]
     public class UserLogInPageTest
     {
+
         [Test]
         public void LogIn_ValidInput_LogInSuccessfully()
         {
             string userName;
             Threaded<Session>
-                .With<Chrome>()
+                .With<GridDriver>()
                 .NavigateTo<UserLogInPage>("http://localhost:8080/user/login")
                 .UserNameField.EnterText("aaron")
                 .PasswordField.EnterText("12345")
-                .DateOfBirth.SelectDate(20)
                 .LoginButton.Click()
                 .UserNameSpan()
                 .Store(out userName, s => s.Text);
---
+++ Tests/SciApp.Web.IntegrationTest/register-selenium-node.ps1
@@ -0,0 +1,1 @@
+java -jar selenium-server-standalone-3.4.0.jar -role node -hub http://192.168.1.105:4444/grid/register -port 5555
