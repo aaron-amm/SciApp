6cb2263b13aef2307556e48399e0a559994b4c39
aaronamm <theeranitp@gmail.com>
2017-05-03 22:35:17 +0700

    updated jQuery, test PhantomJS

--- Tests/SciApp.Web.IntegrationTest/LogInPageTest.cs
+++ Tests/SciApp.Web.IntegrationTest/LogInPageTest.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Threading;
-using Bumblebee.Extensions;
-using Bumblebee.Interfaces;
+﻿using Bumblebee.Extensions;
 using Bumblebee.Setup;
-using Bumblebee.Setup.DriverEnvironments;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.PhantomJS;
 
 namespace SciApp.Web.IntegrationTest
 {
@@ -16,17 +11,19 @@ namespace SciApp.Web.IntegrationTest
     {
         public IWebDriver CreateWebDriver()
         {
-            const string node = "http://192.168.1.105:4444/wd/hub";
-            var cap = DesiredCapabilities.Chrome();
-            //cap.SetCapability("browserName","chrome");
-            //cap.SetCapability("platform","WIN10");
+            //const string node = "http://192.168.1.105:4444/wd/hub";
+            //var cap = DesiredCapabilities.Chrome();
+            ////cap.SetCapability("browserName","chrome");
+            ////cap.SetCapability("platform","WIN10");
+            ////var driver = new RemoteWebDriver(new Uri(node), cap);
+            ////return driver;
+            ////var option = new ChromeOptions();
+            ////option.BinaryLocation = @"C:\projects\SciApp\packages\Selenium.WebDriver.ChromeDriver.2.29.0\driver\win32\chromedriver.exe";
+            ////var capabilities = option.ToCapabilities();
+            ////var commandTimeout = TimeSpan.FromMinutes(5);
             //var driver = new RemoteWebDriver(new Uri(node), cap);
             //return driver;
-            //var option = new ChromeOptions();
-            //option.BinaryLocation = @"C:\projects\SciApp\packages\Selenium.WebDriver.ChromeDriver.2.29.0\driver\win32\chromedriver.exe";
-            //var capabilities = option.ToCapabilities();
-            //var commandTimeout = TimeSpan.FromMinutes(5);
-            var driver = new RemoteWebDriver(new Uri(node), cap);
+            var driver = new PhantomJSDriver();
             return driver;
         }
     }
@@ -37,17 +34,20 @@ namespace SciApp.Web.IntegrationTest
         [Test]
         public void LogIn_ValidInput_LogInSuccessfully()
         {
+            var session = Threaded<Session>.With<GridDriver>();
             string userName;
-            Threaded<Session>
-                .With<GridDriver>()
-                .NavigateTo<UserLogInPage>("http://localhost:8080/user/login")
-                .UserNameField.EnterText("aaron")
-                .PasswordField.EnterText("12345")
-                .LoginButton.Click()
-                .UserNameSpan()
-                .Store(out userName, s => s.Text);
+            session.NavigateTo<UserLogInPage>("http://localhost:8080/user/login")
+            .UserNameField.EnterText("aaron")
+            .PasswordField.EnterText("12345")
+            .LoginButton.Click()
+            .UserNameSpan()
+            .Store(out userName, s => s.Text);
+
+            var takeScreenShort = (ITakesScreenshot)session.Driver;
+            takeScreenShort.GetScreenshot().SaveAsFile("login.jpg", ScreenshotImageFormat.Jpeg);
 
             Assert.AreEqual("aaron", userName);
+
         }
 
         ///The tear down operation is needed in case there is a failure during the test.  The Session will need to be
